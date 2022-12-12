70a3aec098fe1c38069d867faa25b43ec6eae555
Jiawei Lu <jl5999@columbia.edu>
2022-12-11 21:04:56 -0500

    skeleton

---
+++ src/main/java/com/javeylew/stt/SimpleTranslateToolApplication.java
@@ -0,0 +1,13 @@
+package com.javeylew.stt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTranslateToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleTranslateToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javeylew/stt/SimpleTranslateToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javeylew.stt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTranslateToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
